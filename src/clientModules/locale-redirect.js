c2af527b489881757ca7d587b9593e5e5ff7598a
gold-plus <gold-plus@users.noreply.github.com>
2025-08-06 20:14:59 +0700

    fix redirect

--- src/clientModules/locale-redirect.js
+++ src/clientModules/locale-redirect.js
@@ -4,21 +4,31 @@ if (ExecutionEnvironment.canUseDOM) {
     // reading preferred locale before mounting i18n
     const defaultLocale = 'ru';
     const supportedLocales = ['ru', 'en'];
-    const pathMatch = window.location.pathname.match(/^\/([a-z]{2,}(?:-[A-Z]{2})?)(\/|$)/);
-    const currentLocale = pathMatch ? pathMatch[1] : defaultLocale;
+    const parts = window.location.pathname.split('/');
+    const maybeLocale = parts[1];
+    const currentLocale = supportedLocales.includes(maybeLocale)
+      ? maybeLocale
+      : defaultLocale;
     let preferred = localStorage.getItem('preferred_locale');
     if (!preferred) {
       const browserLang = navigator.language || navigator.languages?.[0];
       const shortLang = browserLang.split('-')[0]; // e.g 'en-US' > 'en'
       if (supportedLocales.includes(browserLang)) {
         preferred = browserLang;
-      } else if (supportedLocales.includes(shortLang)) {
+      }
+      else if (supportedLocales.includes(shortLang)) {
         preferred = shortLang;
       }
     }
-    if (preferred && preferred !== currentLocale) {
-      let newPath = window.location.pathname.replace(/^\/[a-z]{2,}(?:-[A-Z]{2})?/, '');
-      if (preferred !== defaultLocale) newPath = '/' + preferred + newPath;
+    if (preferred && preferred !== currentLocale)
+    {
+      let newPath = window.location.pathname;
+      if (supportedLocales.includes(parts[1])) {
+        newPath = '/' + parts.slice(2).join('/');
+      }
+      if (preferred !== defaultLocale) {
+        newPath = '/' + preferred + newPath;
+      }
       // earliest redirect, before i18n init
       window.location.replace(newPath + window.location.search + window.location.hash);
     }
