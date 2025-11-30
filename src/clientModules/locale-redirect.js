898929c32172ce2a5626c3b2611fb4e02eb2ac44
s1lentq <s1lentsk@yandex.ru>
2025-11-30 20:32:58 +0700

    fix(locale-redirect): skip redirect for bots

--- src/clientModules/locale-redirect.js
+++ src/clientModules/locale-redirect.js
@@ -1,37 +1,46 @@
 import ExecutionEnvironment from '@docusaurus/ExecutionEnvironment';
+
+function handleLocaleRedirect() {
+  // reading preferred locale before mounting i18n
+  const defaultLocale = 'ru';
+  const supportedLocales = ['ru', 'en'];
+  const ua = navigator.userAgent || '';
+  if (/bot|crawler|spider|crawling|algolia/i.test(ua)) {
+    return; // skip redirect for all bots and crawlers
+  }
+  const parts = window.location.pathname.split('/');
+  const maybeLocale = parts[1];
+  const currentLocale = supportedLocales.includes(maybeLocale)
+    ? maybeLocale
+    : defaultLocale;
+  let preferred = localStorage.getItem('preferred_locale');
+  if (!preferred) {
+    const browserLang = navigator.language || navigator.languages?.[0];
+    const shortLang = browserLang.split('-')[0]; // e.g 'en-US' > 'en'
+    if (supportedLocales.includes(browserLang)) {
+      preferred = browserLang;
+    }
+    else if (supportedLocales.includes(shortLang)) {
+      preferred = shortLang;
+    }
+  }
+  if (preferred && preferred !== currentLocale)
+  {
+    let newPath = window.location.pathname;
+    if (supportedLocales.includes(parts[1])) {
+      newPath = '/' + parts.slice(2).join('/');
+    }
+    if (preferred !== defaultLocale) {
+      newPath = '/' + preferred + newPath;
+    }
+    // earliest redirect, before i18n init
+    window.location.replace(newPath + window.location.search + window.location.hash);
+  }
+}
+
 if (ExecutionEnvironment.canUseDOM) {
   try {
-    // reading preferred locale before mounting i18n
-    const defaultLocale = 'ru';
-    const supportedLocales = ['ru', 'en'];
-    const parts = window.location.pathname.split('/');
-    const maybeLocale = parts[1];
-    const currentLocale = supportedLocales.includes(maybeLocale)
-      ? maybeLocale
-      : defaultLocale;
-    let preferred = localStorage.getItem('preferred_locale');
-    if (!preferred) {
-      const browserLang = navigator.language || navigator.languages?.[0];
-      const shortLang = browserLang.split('-')[0]; // e.g 'en-US' > 'en'
-      if (supportedLocales.includes(browserLang)) {
-        preferred = browserLang;
-      }
-      else if (supportedLocales.includes(shortLang)) {
-        preferred = shortLang;
-      }
-    }
-    if (preferred && preferred !== currentLocale)
-    {
-      let newPath = window.location.pathname;
-      if (supportedLocales.includes(parts[1])) {
-        newPath = '/' + parts.slice(2).join('/');
-      }
-      if (preferred !== defaultLocale) {
-        newPath = '/' + preferred + newPath;
-      }
-      // earliest redirect, before i18n init
-      window.location.replace(newPath + window.location.search + window.location.hash);
-    }
+    handleLocaleRedirect();
   } catch (e) {
     // stub errors
   }
